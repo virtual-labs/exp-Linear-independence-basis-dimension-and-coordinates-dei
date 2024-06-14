9552acf74aaa0d605b017ea53e4f007ca35b8e41
vleadadmin <admin@vlabs.ac.in>
2024-06-14 04:38:45 +0000

    https://virtual-labs.github.io/virtual-labs/exp-Linear-independence-basis-dimension-and-coordinates-dei click on the link to test your code.

---
+++ procedure.md
@@ -0,0 +1,6 @@
+1. Press the button “Case 1”, “Case 2”, “Case 3”and “Case 4” one by one on the first page for the visualization of the concept of linear independence and linear dependence. In “Case 4”, guess the answer by clicking the appropriate button.
+2. For better understanding, move further and click on “Example 1” and “Example 2” buttons to see the details.
+3. Click on the “Next” button to go to the next page.
+4. Visualize the examples through given diagrams to understand the concepts of basis, dimension and co-ordinates by clicking respective buttons in the first block.
+5. In the next block, click on appropriate buttons to visualize the construction of basis of R<sup>2</sup>.
+6. Click on “Step 1”, “Step 2” and “Step 3” buttons one after the other to check whether a nonempty subset B of R<sup>2</sup> is a basis or not.
---
+++ simulation/js/basis.js
@@ -0,0 +1,46 @@
+function ck21y(){
+    document.getElementById('ck21y').innerHTML='INCORRECT!';
+    document.getElementById('ck21n').innerHTML='';
+}
+function ck21n(){
+    document.getElementById('ck21n').innerHTML='CORRECT!';
+    document.getElementById('ck21y').innerHTML='';
+
+}
+
+function ck22y(){
+    document.getElementById('ck22y').innerHTML='INCORRECT!';
+    document.getElementById('ck22n').innerHTML='';
+
+}
+function ck22n(){
+    document.getElementById('ck22n').innerHTML='CORRECT!';
+    document.getElementById('ck22y').innerHTML='';
+
+}
+
+function ck23y(){
+    document.getElementById('ck23y').innerHTML='INCORRECT!';
+    document.getElementById('ck23n').innerHTML='';
+
+}
+function ck23n(){
+    document.getElementById('ck23n').innerHTML='CORRECT!';
+    document.getElementById('ck23y').innerHTML='';
+
+}
+
+function reason(){
+    document.getElementById('reason').innerHTML='If |B| &ne; 2, B is not basis of R<sup>2</sup>';
+}
+
+function exmp1(){
+   
+    document.getElementById('exmpRes1').style.display= "";
+    document.getElementById('eximg1').src='images/bex1.PNG';
+}
+function exmp2(){
+   
+    document.getElementById('exmpRes2').style.display= "";
+    document.getElementById('eximg2').src='images/bex2.png';
+}
